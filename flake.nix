e031e6e7fc4931c9ba0f7bbebb1e8983379e42bf
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-08-03 14:17:29 +0100

    feat(flake): added flake.nix

--- flake.nix
+++ flake.nix
@@ -6,35 +6,38 @@
   outputs =
     { self, nixpkgs }:
     {
-      devShells.default =
-        let
-          system = builtins.currentSystem;
-          pkgs = import nixpkgs { inherit system; };
-        in
-        pkgs.mkShell {
-          name = "mermaid-converter-env";
+      devShells = {
+        x86_64-linux =
+          let
+            pkgs = import nixpkgs { system = "x86_64-linux"; };
+          in
+          {
+            default = pkgs.mkShell {
+              name = "mermaid-converter-env";
 
-          buildInputs = with pkgs; [
-            (python3.withPackages (
-              ps: with ps; [
-                pip
-                setuptools
-              ]
-            ))
-            nodejs_20
-            nodePackages.npm
-            nodePackages.mermaid-cli
-            which
-            coreutils
-            gnused
-            chromium
-          ];
+              buildInputs = with pkgs; [
+                (python3.withPackages (
+                  ps: with ps; [
+                    pip
+                    setuptools
+                  ]
+                ))
+                nodejs_20
+                nodePackages.npm
+                nodePackages.mermaid-cli
+                which
+                coreutils
+                gnused
+                chromium
+              ];
 
-          shellHook = ''
-            export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-            export PUPPETEER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
-            export PATH="$PATH:${pkgs.nodePackages.mermaid-cli}/bin"
-          '';
-        };
+              shellHook = ''
+                export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+                export PUPPETEER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
+                export PATH="$PATH:${pkgs.nodePackages.mermaid-cli}/bin"
+              '';
+            };
+          };
+      };
     };
 }
