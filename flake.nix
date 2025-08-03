f2952891282d555a0deafb3cfc7df494279e8ba3
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-08-03 14:09:01 +0100

    feat(flake): added flake instead of standalone shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Minimal flake shell for Mermaid conversion";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.default =
+        let
+          system = builtins.currentSystem;
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          name = "mermaid-converter-env";
+
+          buildInputs = with pkgs; [
+            (python3.withPackages (
+              ps: with ps; [
+                pip
+                setuptools
+              ]
+            ))
+            nodejs_20
+            nodePackages.npm
+            nodePackages.mermaid-cli
+            which
+            coreutils
+            gnused
+            chromium
+          ];
+
+          shellHook = ''
+            export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+            export PUPPETEER_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
+            export PATH="$PATH:${pkgs.nodePackages.mermaid-cli}/bin"
+          '';
+        };
+    };
+}
